5964c16081b7ae03160cf51bfc42e4d81adc3960
Patrick Etcheverry <patrick.etcheverry@iutbayonne.univ-pau.fr>
2024-10-29 20:56:52 +0100

    Création du workspace avec 9 dossiers pour 9 exercices

---
+++ exercice4/main.cpp
@@ -0,0 +1,19 @@
+/*
+  Numéro du groupe :
+  Nom du groupe :
+  Étudiants :
+  Numéro de Kata : (1 ou 2)
+  Numéro de l'exercice : 4
+  But de l'exercice :
+  Modèle(s) d'algorithme(s) choisi(s) :
+  Brève justification du ou des modèles choisis :
+  Remarques éventuelles :
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    return 0;
+}
